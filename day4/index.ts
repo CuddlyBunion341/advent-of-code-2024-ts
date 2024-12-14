22b9521404fc8b830fc695362f076853a2aa08f5
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-14 19:29:59 +0100

    Refactor

--- day4/index.ts
+++ day4/index.ts
@@ -1,24 +1,17 @@
 const file = await Bun.file("./input").text()
-const rows = file.split("\n")
-const size = rows[0].length
-while (rows.length > size) rows.pop()
+const lines = file.split("\n")
+const size = Math.min(lines.length, lines[0].length)
+const rows = lines.slice(0, size)
 
 const TERM = "XMAS"
 
 const transpose = (matrix: String[]) => matrix.map((s, i) => s.split("").map((_, j) => matrix[j][i]).join(""))
 const reverse = (str: String) => str.split("").reverse().join("")
-const stRange = (start: number, end: number, call: (index: number) => string) => {
-  return Array.from({ length: end - start }).map((_, i) => call(start + i)).join("")
-}
-const range = (count: number, call: (index: number) => string) => stRange(0, count, call)
-const getMatches = (str: String) => [...str.matchAll(/XMAS/g)]
-const removeNonMatches = (str: String) => {
-  return [...str.split("")].map((v, i) => getMatches(str).some(({ index }) => i >= index && i < index + TERM.length) ? v : '.')
-}
+const rangeStr = (count: number, call: (i: number) => string) => Array.from({ length: count }).map((_, i) => call(i)).join("")
+const getMatches = (str: String) => [...str.matchAll(new RegExp(TERM, "g"))]
+const range = (length: number, call: (i: number) => any) => Array.from({length}).map((_, i) => call(i))
 
 const xmasStrings: String[] = []
-
-const limit = TERM.length - 1
 
 const matricies = [
   rows,
@@ -27,18 +20,18 @@ const matricies = [
   transpose(rows.map(reverse)),
 ]
 
-for (let a = 0; a < size - limit; a++) {
+range(size - TERM.length - 1, (a) => {
   matricies.forEach((matrix, mi) => {
     if (mi % 2 === 0 && a === 0) return
-    xmasStrings.push(range(size, (i) => matrix[i][a + i]))
+    xmasStrings.push(rangeStr(size, (i) => matrix[i][a + i]))
   })
-}
+})
 
 for (let a = 0; a < size; a++) {
   xmasStrings.push(matricies[0][a])
   xmasStrings.push(matricies[1][a])
 }
 
-const totalMatches = xmasStrings.map(v => [v, reverse(v)].map(n => getMatches(n).length).reduce((a,v) => a + v)).reduce((sum, val) => sum + val)
+const totalMatches = xmasStrings.map(v => [v, reverse(v)].map(n => getMatches(n).length).reduce((a, v) => a + v)).reduce((sum, val) => sum + val)
 
 console.log(totalMatches)
