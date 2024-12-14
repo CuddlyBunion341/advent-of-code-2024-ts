b64347725a9ae1c7daf8f9dc099a1e519717e65d
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-14 19:38:02 +0100

    Revert "Attempt"
    This reverts commit b95bb76edfff8c87b4cf9091c916cfcd5a52bc7f.

--- day4/index.ts
+++ day4/index.ts
@@ -1,4 +1,4 @@
-const file = await Bun.file("./input2").text()
+const file = await Bun.file("./input").text()
 const lines = file.split("\n")
 const size = Math.min(lines.length, lines[0].length)
 const rows = lines.slice(0, size)
@@ -7,8 +7,11 @@ const TERM = "XMAS"
 
 const transpose = (matrix: String[]) => matrix.map((s, i) => s.split("").map((_, j) => matrix[j][i]).join(""))
 const reverse = (str: String) => str.split("").reverse().join("")
+const rangeStr = (count: number, call: (i: number) => string) => Array.from({ length: count }).map((_, i) => call(i)).join("")
 const getMatches = (str: String) => [...str.matchAll(new RegExp(TERM, "g"))]
 const range = (length: number, call: (i: number) => any) => Array.from({length}).map((_, i) => call(i))
+
+const xmasStrings: String[] = []
 
 const matricies = [
   rows,
@@ -17,18 +20,17 @@ const matricies = [
   transpose(rows.map(reverse)),
 ]
 
-const xmasStrings: String[] = [
-  ...range(size - TERM.length - 1, (a) => (
-    matricies.map((matrix, mi) => {
-      if (mi % 2 === 0 && a === 0) return
-      return range(size, (i) => matrix[i][a + i]).join("")
-    })
-  )).flat(),
-  ...range(size, a => matricies[0][a]),
-  ...range(size, a => matricies[1][a]),
-].filter(v => v)
+range(size - TERM.length - 1, (a) => {
+  matricies.forEach((matrix, mi) => {
+    if (mi % 2 === 0 && a === 0) return
+    xmasStrings.push(rangeStr(size, (i) => matrix[i][a + i]))
+  })
+})
 
-  console.log(xmasStrings)
+for (let a = 0; a < size; a++) {
+  xmasStrings.push(matricies[0][a])
+  xmasStrings.push(matricies[1][a])
+}
 
 const totalMatches = xmasStrings.map(v => [v, reverse(v)].map(n => getMatches(n).length).reduce((a, v) => a + v)).reduce((sum, val) => sum + val)
 
